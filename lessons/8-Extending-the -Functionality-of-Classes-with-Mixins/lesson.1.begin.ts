c845687ba3e0721e52d317309a42c7f4206be817
Obinna Ogbonna <obinnaogbonnajoseph@gmail.com>
2022-08-13 15:53:23 +0100

    use mixins

--- lessons/8-Extending-the -Functionality-of-Classes-with-Mixins/lesson.1.begin.ts
+++ lessons/8-Extending-the -Functionality-of-Classes-with-Mixins/lesson.1.begin.ts
@@ -1,40 +1,55 @@
 import { Project, Size } from "./types";
 
-class ApiClient {
-  baseUrl: string;
+export class CoreAPI {
+  constructor(public baseUrl: string){}
+}
 
-  constructor(baseUrl: string) {
-    this.baseUrl = baseUrl;
-  }
+export type BaseApiConstructor = new (...args: any[]) => CoreAPI;
 
-  getProjects(): Promise<Project[]> {
-    return Promise.resolve([]);
-  }
-
-  saveProject(): Promise<string> {
-    return Promise.resolve("1");
-  }
-
-  deleteProject(): Promise<void> {
-    return Promise.resolve();
-  }
-
-  getImages(): Promise<{
-    src: string;
-    dimensions: Size;
-  }> {
-    return Promise.resolve({
-      src: "http://a-fake-url.com/image-id",
-      dimensions: {
-        height: 100,
-        width: 300
-      }
-    });
-  }
-
-  saveImage(src: string): Promise<number> {
-    return Promise.resolve(10);
+function ImagesAPI<TBase extends BaseApiConstructor>(Base: TBase) {
+  return class extends Base {
+    getImages(): Promise<{
+      src: string;
+      dimensions: Size;
+    }> {
+      return Promise.resolve({
+        src: "http://a-fake-url.com/image-id",
+        dimensions: {
+          height: 100,
+          width: 300
+        }
+      });
+    }
+  
+    saveImage(src: string): Promise<number> {
+      return Promise.resolve(10);
+    }
   }
 }
 
+function ProjectsAPI<TBase extends BaseApiConstructor>(Base: TBase) {
+  return class extends Base {
+    getProjects(): Promise<Project[]> {
+      return Promise.resolve([]);
+    }
+  
+    saveProject(): Promise<string> {
+      return Promise.resolve("1");
+    }
+  
+    deleteProject(): Promise<void> {
+      return Promise.resolve();
+    }
+  }
+}
+
+class ApiClient extends ProjectsAPI(ImagesAPI(CoreAPI)) {
+
+}
+
 const api = new ApiClient("http://pluralsight.com");
+api.saveProject();
+api.saveImage("hello.png");
+api.deleteProject();
+api.getImages();
+api.getProjects();
